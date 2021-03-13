3a81daf092e4ac167d2442af9dc5a4a37d2d9b19
VinoLoser <vinowang921@gmail.com>
2021-03-13 14:40:26 -0500

    Add new files

--- EulerProject/EulerProject53/Main.java
+++ EulerProject/EulerProject53/Main.java
@@ -4,25 +4,17 @@ public class Main {
 
     static int amount = 0;
     public static void main(String[] args) {
-        int[][] array = new int[101][101];
+        int[] array = new int[101];
         for (int m = 0; m <= 100; m++) {
-            for (int n = 0; n <= m; n++) {
+            for (int n = m; n >= 0; n--) {
                 if (m == n || n == 0) {
-                    array[m][n] = 1;
+                    array[n] = 1;
                 } else {
-                    array[m][n] = Math.min(1000001, array[m-1][n-1] + array[m-1][n]);
-                    amount += array[m][n] > 1000000 ? 1 : 0;
+                    array[n] = Math.min(1000001, array[n-1] + array[n]);
+                    amount += array[n] > 1000000 ? 1 : 0;
                 }
             }
         }
         System.out.println(amount);
-        /*
-        for (int temp = 0; temp < array[0].length; temp++) {
-            for (int i : array[temp]) {
-                System.out.print(i);
-            }
-            System.out.println();
-        }
-        */
     }
 }
